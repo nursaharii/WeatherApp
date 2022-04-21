8d5558795aba8b9431d2680394a47791d8b94258
Nurşah ARİ <33926714+nursaharii@users.noreply.github.com>
2022-04-21 13:51:02 +0300

    Update URLExtension.swift

--- WeatherApp/Extensions/URLExtension.swift
+++ WeatherApp/Extensions/URLExtension.swift
@@ -9,12 +9,12 @@ import Foundation
 
 extension URL {
     static func weatherCityUrl(city: String) -> String {
-        return  "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=cId7nh27mDivBhAO3s9u4cuBqCiLihbu&q=\(city)"
+        return  "http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q=\(city)"
     }
     static func localCoordinateUrl(lat: String,lon: String)-> String {
-        return "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=cId7nh27mDivBhAO3s9u4cuBqCiLihbu&q=\(lat)%2C\(lon)"
+        return "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey={api_key}&q=\(lat)%2C\(lon)"
     }
     static func hourlyForecasts(key: String) -> String {
-        return "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/\(key)?apikey=cId7nh27mDivBhAO3s9u4cuBqCiLihbu"
+        return "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/\(key)?apikey={api_key}"
     }
 }
